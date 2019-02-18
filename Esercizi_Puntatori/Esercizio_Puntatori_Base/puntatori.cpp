98714cc9867d2876b49f5b6e5d7c05c9c95d3fd0
Eodardo Viganò <edo.vigano.29@gmail.com>
2019-02-18 12:43:37 +0100

    Esercizio Iniziale Puntatori

--- Esercizi_Puntatori/Esercizio_Puntatori_Base/puntatori.cpp
+++ Esercizi_Puntatori/Esercizio_Puntatori_Base/puntatori.cpp
@@ -4,7 +4,7 @@
 //alla posizione 0 viene messo il path dell'eseguibile
 
 
-void stampaStringa(const char *p) {
+void stampaStringa(char *p) {
   while(*p != '\0')
   {
     std::cout << *p;
@@ -13,7 +13,7 @@ void stampaStringa(const char *p) {
   std::cout << '\n';
 }
 
-int lunghezzaStringa(const char *p) {
+int lunghezzaStringa(char *p) {
   int length = 0;
   while (*p != '\0') {
     length++;
@@ -22,6 +22,63 @@ int lunghezzaStringa(const char *p) {
   return length;
 }
 
+void stampaInvertita( char *p)
+{
+  int x = lunghezzaStringa(p);
+  while (*p != '\0') {
+    p++;
+  }
+
+  for (int i = x ; i >= 0;  i--) {
+    std::cout << *p;
+    p--;
+  }
+  std::cout << '\n';
+}
+
+
+/*void reverse(char *p) {
+  char *str = p - 1 + lunghezzaStringa(p);
+  char c;
+
+
+  while (p < str) {
+    c = *p;
+    *p = *str;
+    *str = c;
+    p++;
+    str--;
+  }
+}
+*/
+
+char *copy(char *p){
+  char *p1 = new char[lunghezzaStringa(p)+1];
+  for (int i = 0; i <= lunghezzaStringa(p); i++)
+  {
+    p1[i] = p[i];
+  }
+  return p1;
+}
+
+char *concatena(char *p1, char *p2) {
+  int l1 = lunghezzaStringa(p1);
+  int l2 = lunghezzaStringa(p2);
+
+  char *p3 = new char[l1+l2+1];
+
+  for (int i = 0; i < l1; i++)
+  {
+    p3[i] = p1[i];
+  }
+
+  for (int i = l1; i < l1+l2; i++)
+  {
+    p3[i] = p2[i-l1];
+  }
+
+  return p3;
+}
 
 
 
@@ -29,18 +86,29 @@ int lunghezzaStringa(const char *p) {
 
 
 
-
-
-
-
-
-int main(int argc, char const *argv[]) {
+int main(int argc, char *argv[]) {
 
   //std::cout << argv[1] << '\n';
 
-  const char *p = argv[1];
+  char *p = argv[1];
   stampaStringa(p);
   std::cout << "La lunghezza della stringa e': " << lunghezzaStringa(p) << '\n';
-  
+  std::cout << "Stamapa invertita:";
+  stampaInvertita(p);
+
+  char *p1 = copy(p);
+  std::cout << "Stringa copiata: ";
+  stampaStringa(p1);
+  char *p2 = argv[2];
+  stampaStringa(p2);
+  char *p3 = concatena(p1, p2);
+  std::cout << "String concatenata: ";
+  stampaStringa(p3);
+
+
+
+
+
+
   return 0;
 }
